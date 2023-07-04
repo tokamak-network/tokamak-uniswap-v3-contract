31191f7f869c5739a281423fef7202fd5865fcbf
justin_g <usgeeus@gmail.com>
2023-07-04 10:41:44 +0900

    swap_test

--- scripts/v3/mainnet/6.swap_test.js
+++ scripts/v3/mainnet/6.swap_test.js
@@ -26,6 +26,7 @@ async function main() {
   ///=============== SwapRouterContract  
   const SwapRouterContract = await getContract('SwapRouter02');
   const SwapRouterAddress = SwapRouterContract.address;
+  console.log(SwapRouterAddress);
 
   let poolAddressTOSTON = await getPoolContractAddress(UniswapV3FactoryContract, TONAddress, TOSAddress, 3000);
   let poolAddressWETHTON = await getPoolContractAddress(UniswapV3FactoryContract, WETHAddress, TONAddress, 3000);
@@ -42,7 +43,7 @@ async function main() {
 
   let deadline = Date.now() + 100000;
 
-  let amountIn = ethers.utils.parseEther('1');
+  let amountIn = ethers.utils.parseEther('500');
   //==============TOS => TOS (ERC20->ERC20)
   let SwapParams = 
     {
@@ -72,68 +73,93 @@ async function main() {
       console.log("e", e.message);
     }
 
-  // //==============ETH => TON (ETH->ERC20)
-  // amountIn = ethers.utils.parseEther('0.000804204211611148');
-  // SwapParams = 
-  //   {
-  //     tokenIn: WETHAddress,
-  //     tokenOut: TONAddress,
-  //     fee: 3000,
-  //     recipient: deployer.address,
-  //     deadline: deadline,
-  //     amountIn: amountIn,
-  //     amountOutMinimum: 0,
-  //     sqrtPriceLimitX96: 0,
-  //   }
-  // ;
-  // try{
-  //     const tx = await SwapRouterContract.exactInputSingle(SwapParams, {
-  //       gasLimit: 3000000, value: amountIn
-  //     });
-  //     await tx.wait();
-  //     const receipt = await providers.getTransactionReceipt(tx.hash);
-  //     console.log("===ETH => TON (ETH->ERC20)");
-  //     console.log("transactionHash:", receipt.transactionHash);
-  //     console.log("gasUsed: ",receipt.gasUsed);
-  //     console.log();
-  //     totalGasUsed = totalGasUsed.add(receipt.gasUsed);
-  //   }
-  //   catch(e) {
-  //     console.log("e", e.message);
-  //   }
+  //==============ETH => TON (ETH->ERC20)
+  amountIn = ethers.utils.parseEther('0.000804204211611148');
+  SwapParams = 
+    {
+      tokenIn: WETHAddress,
+      tokenOut: TONAddress,
+      fee: 3000,
+      recipient: deployer.address,
+      deadline: deadline,
+      amountIn: amountIn,
+      amountOutMinimum: 0,
+      sqrtPriceLimitX96: 0,
+    }
+  ;
+  try{
+      const tx = await SwapRouterContract.exactInputSingle(SwapParams, {
+        gasLimit: 3000000, value: amountIn
+      });
+      await tx.wait();
+      const receipt = await providers.getTransactionReceipt(tx.hash);
+      console.log("===ETH => TON (ETH->ERC20)");
+      console.log("transactionHash:", receipt.transactionHash);
+      console.log("gasUsed: ",receipt.gasUsed);
+      console.log();
+      totalGasUsed = totalGasUsed.add(receipt.gasUsed);
+    }
+    catch(e) {
+      console.log("e", e.message);
+    }
 
-  // //==============TON => ETH (ERC20->ETH)
-  // amountIn = ethers.utils.parseEther('1');
-  // const params1 = 
-  //   {
-  //     tokenIn: TONAddress,
-  //     tokenOut: WETHAddress,
-  //     fee: 3000,
-  //     recipient: SwapRouterAddress,
-  //     deadline: deadline,
-  //     amountIn: amountIn,
-  //     amountOutMinimum: 0,
-  //     sqrtPriceLimitX96: 0,
-  //   }
-  // ;
-  // const encData1 = SwapRouterContract.interface.encodeFunctionData('exactInputSingle', [params1]);
-  // const amountMinimum = 0
-  // const encData2 = SwapRouterContract.interface.encodeFunctionData('unwrapWETH9', [amountMinimum, deployer.address])
-  // try{
-  //     const tx = await SwapRouterContract.multicall([encData1, encData2], {
-  //       gasLimit: 3000000
-  //     });
-  //     await tx.wait();
-  //     const receipt = await providers.getTransactionReceipt(tx.hash);
-  //     console.log("===TON => ETH (ERC20->ETH)");
-  //     console.log("transactionHash:", receipt.transactionHash);
-  //     console.log("gasUsed: ",receipt.gasUsed);
-  //     console.log();
-  //     totalGasUsed = totalGasUsed.add(receipt.gasUsed);
-  //   }
-  //   catch(e) {
-  //     console.log("e", e.message);
-  //   }
+  //==============TON => ETH (ERC20->ETH)
+  amountIn = ethers.utils.parseEther('1');
+  const params1 = 
+    {
+      tokenIn: TONAddress,
+      tokenOut: WETHAddress,
+      fee: 3000,
+      recipient: SwapRouterAddress,
+      deadline: deadline,
+      amountIn: amountIn,
+      amountOutMinimum: 0,
+      sqrtPriceLimitX96: 0,
+    }
+  ;
+  const encData1 = SwapRouterContract.interface.encodeFunctionData('exactInputSingle', [params1]);
+  const amountMinimum = 0
+  const encData2 = SwapRouterContract.interface.encodeFunctionData('unwrapWETH9(uint256,address)', [amountMinimum, deployer.address])
+
+  const encMultiCall = SwapRouterContract.interface.encodeFunctionData('multicall(uint256,bytes[])', [deadline,[encData1, encData2]])
+  //console.log(encMultiCall);
+  deadline = Math.floor(Date.now() / 1000) + 100000;
+  try {
+    // const tx = await SwapRouterContract.multicall(swapData, {gasLimit:300000});
+    //const tx = await SwapRouterContract.multicall(swapData, {gasLimit:300000});
+    const txArgs = {
+        to: SwapRouterContract.address,
+        from: deployer.address,
+        data: encMultiCall,
+        gasLimit: 300000,
+    }
+    const tx = await deployer.sendTransaction(txArgs)
+    await tx.wait();
+    const receipt = await providers.getTransactionReceipt(tx.hash);
+    console.log("===TON => ETH (ETH->ERC20)");
+    console.log("transactionHash:", receipt.transactionHash);
+    console.log("gasUsed: ",receipt.gasUsed);
+    console.log();
+    totalGasUsed = totalGasUsed.add(receipt.gasUsed);
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  try{
+      const tx = await SwapRouterContract.multicall([encData1, encData2], {
+        gasLimit: 3000000
+      });
+      await tx.wait();
+      const receipt = await providers.getTransactionReceipt(tx.hash);
+      console.log("===TON => ETH (ERC20->ETH)");
+      console.log("transactionHash:", receipt.transactionHash);
+      console.log("gasUsed: ",receipt.gasUsed);
+      console.log();
+      totalGasUsed = totalGasUsed.add(receipt.gasUsed);
+    }
+    catch(e) {
+      console.log("e", e.message);
+    }
     console.log("totalGasUsed:",totalGasUsed);
 }
 
