738474c463f776995ca7f0972155c2db1b578100
지의신 <64007655+usgeeus@users.noreply.github.com>
2023-10-03 02:31:43 +0900

    Update 2.approve_nftManager.js

--- scripts/v3/mainnet/2.approve_nftManager.js
+++ scripts/v3/mainnet/2.approve_nftManager.js
@@ -1,44 +1,45 @@
-const ethers = require('ethers');
-require('dotenv').config();
-const hre = require('hardhat');
-const fs = require('fs');
+const ethers = require("ethers");
+require("dotenv").config();
+const hre = require("hardhat");
+const fs = require("fs");
 const {
   getContract,
   getPoolContractAddress,
-} = require('./helper_functions.js');
-const { expect } = require('chai');
+} = require("./helper_functions.js");
+const { expect } = require("chai");
 
 const chainName = hre.network.name;
-let allowanceAmount = ethers.utils.parseEther('1000000000000'); //0 12개 ether
-let minimumallowanceAmount = ethers.utils.parseEther('100000000000'); //0 11개 ether
+//let allowanceAmount = ethers.utils.parseEther("1000000000000"); //0 12개
+let allowanceAmount = ethers.constants.MaxUint256;
+let minimumallowanceAmount = ethers.constants.MaxUint256; //0 11개 ether
 
 async function main() {
   const accounts = await hre.ethers.getSigners();
   let deployer = accounts[0];
   providers = hre.ethers.provider;
-  let totalGasUsed = ethers.BigNumber.from('0');
-  if (chainName === 'hardhat')
+  let totalGasUsed = ethers.BigNumber.from("0");
+  if (chainName === "hardhat")
     deployer = await hre.ethers.getImpersonatedSigner(
-      '0xB68AA9E398c054da7EBAaA446292f611CA0CD52B'
+      "0xB68AA9E398c054da7EBAaA446292f611CA0CD52B"
     );
   ///=============== NonfungiblePositionManagerContract
   const NonfungiblePositionManagerContract = await getContract(
-    'NonfungiblePositionManager'
+    "NonfungiblePositionManager"
   );
   const NonfungiblePositionManagerAddress =
     NonfungiblePositionManagerContract.address;
   ///=============== TONContract
-  const TONContract = (await getContract('TON')).connect(deployer);
+  const TONContract = (await getContract("TON")).connect(deployer);
   ///=============== TOSContract
-  const TOSContract = (await getContract('TOS')).connect(deployer);
+  const TOSContract = (await getContract("TOS")).connect(deployer);
   ///=============== USDCContract
-  const USDCContract = (await getContract('USDC')).connect(deployer);
+  const USDCContract = (await getContract("USDC")).connect(deployer);
   ///=============== USDTContract
-  const USDTContract = (await getContract('USDT')).connect(deployer);
+  const USDTContract = (await getContract("USDT")).connect(deployer);
 
   totalGasUsed = totalGasUsed.add(
     await allowFunction(
-      'TON',
+      "TON",
       TONContract,
       deployer.address,
       NonfungiblePositionManagerAddress
@@ -46,7 +47,7 @@ async function main() {
   );
   totalGasUsed = totalGasUsed.add(
     await allowFunction(
-      'TOS',
+      "TOS",
       TOSContract,
       deployer.address,
       NonfungiblePositionManagerAddress
@@ -54,7 +55,7 @@ async function main() {
   );
   totalGasUsed = totalGasUsed.add(
     await allowFunction(
-      'USDC',
+      "USDC",
       USDCContract,
       deployer.address,
       NonfungiblePositionManagerAddress
@@ -62,13 +63,13 @@ async function main() {
   );
   totalGasUsed = totalGasUsed.add(
     await allowFunction(
-      'USDT',
+      "USDT",
       USDTContract,
       deployer.address,
       NonfungiblePositionManagerAddress
     )
   );
-  console.log('totalGasUsded:', totalGasUsed);
+  console.log("totalGasUsded:", totalGasUsed);
 }
 
 async function allowFunction(tokenName, tokenContract, sender, spender) {
@@ -80,14 +81,14 @@ async function allowFunction(tokenName, tokenContract, sender, spender) {
     expect(await tokenContract.allowance(sender, spender)).to.equal(
       allowanceAmount
     );
-    console.log(`${tokenName} ${allowanceAmount} * 10e18 amount Approved`);
+    console.log(`${tokenName} ${allowanceAmount} amount Approved`);
     const receipt = await providers.getTransactionReceipt(tx.hash);
-    console.log('transactionHash:', receipt.transactionHash);
-    console.log('gasUsed: ', receipt.gasUsed);
+    console.log("transactionHash:", receipt.transactionHash);
+    console.log("gasUsed: ", receipt.gasUsed);
     console.log();
     return receipt.gasUsed;
   } else {
-    return ethers.BigNumber.from('0');
+    return ethers.BigNumber.from("0");
   }
 }
 
